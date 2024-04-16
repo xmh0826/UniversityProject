88a38a7cff56060256eaf650291c970f095ad1a7
xmh <1311364121@qq.com>
2024-04-16 11:52:17 +0800

    element主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// 导入对应包
+import ElementPlus from 'unplugin-element-plus/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,12 +19,29 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 1. 配置elementPlus采用 sass样式 配色系统
+        ElementPlusResolver({importStyle:"sass"})
+      ],
+    }),
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
