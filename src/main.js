f9d286246ef102279a582d0667e6f313ff933914
xmh <1311364121@qq.com>
2024-04-19 21:08:30 +0800

    登录-Pinia 数据持久化

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
@@ -16,8 +16,11 @@ import { lazyPlugin } from'@/directives/index'
 import { componentPlugin } from './components/index'
 
 const app = createApp(App)
+const pinia = createPinia()
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
