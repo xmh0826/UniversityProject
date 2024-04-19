38069e959c27e55c5aad03de999cf40debe9ba1a
xmh <1311364121@qq.com>
2024-04-19 15:17:02 +0800

    图片预览组件优化

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -2,14 +2,14 @@
 import { ref, watch } from "vue"
 import { useMouseInElement } from '@vueuse/core'
 
-// 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+const props = defineProps({
+  imageList:{
+    type:Array,
+    // 默认值
+    default:() => []
+  }
+})
+
 
 // 小图切换大图显示
 const activeIndex = ref(0) 
@@ -66,13 +66,13 @@ watch([elementX, elementY, isOutside], () => {
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
-      <img :src="imageList[activeIndex]" alt="" />
+      <img :src="props?.imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
       <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
-      <li v-for="(img, i) in imageList" :key="i" @mouseenter="enterhandler(i)" :class="{active: i === activeIndex}">
+      <li v-for="(img, i) in props.imageList" :key="i" @mouseenter="enterhandler(i)" :class="{active: i === activeIndex}">
         <img :src="img" alt="" />
       </li>
     </ul>
